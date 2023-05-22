b5ee9ebc922740e87faaf34134dd7545d01247a8
vladamori <vladamori@example.com>
2023-05-22 13:04:44 +0300

    lab004

---
+++ p004layouts/src/main/java/com/poyata/p004layouts/MainActivity.java
@@ -0,0 +1,13 @@
+package com.poyata.p004layouts;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.rlayout);
+    }
+}
